ffb902879c109786b8b7f7631c282c08ac9d0eeb
robertomoutinho <robertomoutinho@gmail.com>
2021-06-07 13:52:51 -0300

    fixed sha256

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -4,7 +4,7 @@ class TerragruntAtlantisConfig < Formula
   url "https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/v1.5.1/terragrunt-atlantis-config_1.5.1_darwin_amd64.zip"
   version "0.10.1"
   # Can be calculated with `curl <url_from_above> --output /tmp/tac && shasum -a256 /tmp/tac | awk '{ print $1 }'`, but only after it is already hosted
-  sha256 "4043ea179954272dd44b2fd7c0eabe19ca41e97bee4927cfd808bea0ed43b056"
+  sha256 "4eb3001c45ab551b4bde26535c581197a70249b06e7e729ef731bf96b3ceab2a"
 
   def install
     bin.install "terragrunt-atlantis-config_1.5.1_darwin_amd64" => "terragrunt-atlantis-config"
