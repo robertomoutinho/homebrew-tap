a09f33f98ac3f2947469e4b78896c60a71034838
robertomoutinho <robertomoutinho@gmail.com>
2021-06-07 13:56:03 -0300

    fixed version

--- Formula/terragrunt-atlantis-config.rb
+++ Formula/terragrunt-atlantis-config.rb
@@ -2,7 +2,7 @@ class TerragruntAtlantisConfig < Formula
   desc "Generates Atlantis Config for Terragrunt projects"
   homepage "https://github.com/transcend-io/terragrunt-atlantis-config"
   url "https://github.com/transcend-io/terragrunt-atlantis-config/releases/download/v1.5.1/terragrunt-atlantis-config_1.5.1_darwin_amd64.zip"
-  version "0.10.1"
+  version "1.5.1"
   # Can be calculated with `curl <url_from_above> --output /tmp/tac && shasum -a256 /tmp/tac | awk '{ print $1 }'`, but only after it is already hosted
   sha256 "4eb3001c45ab551b4bde26535c581197a70249b06e7e729ef731bf96b3ceab2a"
 
